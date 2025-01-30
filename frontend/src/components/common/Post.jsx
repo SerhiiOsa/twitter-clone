2a2687606fd80ec2936a160b155c21f025e10391
Serhii Osa <oserdevit@gmail.com>
2025-01-30 15:14:00 +0200

    3-comment on post mutation added

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -8,11 +8,17 @@ import { Link } from 'react-router-dom';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import toast from 'react-hot-toast';
 import LoadingSpinner from './LoadingSpinner';
+import { formatPostDate } from '../../utils/date';
 
 const Post = ({ post }) => {
   const [comment, setComment] = useState('');
   const { data: authUser } = useQuery({ queryKey: ['authUser'] });
   const queryClient = useQueryClient();
+
+  const postOwner = post.user;
+  const isLiked = post.likes.includes(authUser._id);
+  const isMyPost = post.user._id === authUser._id;
+  const formattedDate = formatPostDate(post.createdAt);
 
   const { mutate: deletePost, isPending: isDeleting } = useMutation({
     mutationFn: async () => {
@@ -62,13 +68,47 @@ const Post = ({ post }) => {
         });
       });
     },
+    onError: (error) => {
+      toast.error(error.message);
+    },
   });
 
-  const postOwner = post.user;
-  const isLiked = post.likes.includes(authUser._id);
-  const isMyPost = post.user._id === authUser._id;
-  const formattedDate = '1h';
-  const isCommenting = false;
+  const { mutate: commentPost, isPending: isCommenting } = useMutation({
+    mutationFn: async () => {
+      try {
+        const response = await fetch('/api/posts/comment/' + post._id, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ text: comment }),
+        });
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error(data.error || 'Failed to comment post');
+        }
+        return data;
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    },
+    onSuccess: (commentedPost) => {
+      queryClient.setQueryData(['posts'], (oldData) => {
+        return oldData.map((p) => {
+          if (p._id === commentedPost._id) {
+            return { ...p, comments: commentedPost.comments };
+          }
+          return p;
+        });
+      });
+      setComment('');
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
 
   const handleDeletePost = () => {
     deletePost();
@@ -76,6 +116,10 @@ const Post = ({ post }) => {
 
   const handlePostComment = (e) => {
     e.preventDefault();
+    if (isCommenting) {
+      return;
+    }
+    commentPost();
   };
 
   const handleLikePost = () => {
