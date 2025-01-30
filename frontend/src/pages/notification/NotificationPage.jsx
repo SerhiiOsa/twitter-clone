a7d88898c5926b331a7ff80f588c3c50f5c3698a
Serhii Osa <oserdevit@gmail.com>
2025-01-30 16:01:13 +0200

    3-notifications querry and mutation added

--- frontend/src/pages/notification/NotificationPage.jsx
+++ frontend/src/pages/notification/NotificationPage.jsx
@@ -1,35 +1,63 @@
 import { Link } from 'react-router-dom';
-import LoadingSpinner from '../../components/common/LoadingSpinner';
-
 import { IoSettingsOutline } from 'react-icons/io5';
 import { FaUser } from 'react-icons/fa';
 import { FaHeart } from 'react-icons/fa6';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+import LoadingSpinner from '../../components/common/LoadingSpinner';
 
 const NotificationPage = () => {
-  const isLoading = false;
-  const notifications = [
-    {
-      _id: '1',
-      from: {
-        _id: '1',
-        username: 'johndoe',
-        profileImg: '/avatars/boy2.png',
-      },
-      type: 'follow',
-    },
-    {
-      _id: '2',
-      from: {
-        _id: '2',
-        username: 'janedoe',
-        profileImg: '/avatars/girl1.png',
-      },
-      type: 'like',
-    },
-  ];
+  const queryClient = useQueryClient();
 
-  const deleteNotifications = () => {
-    alert('All notifications deleted');
+  const { data: notifications, isLoading } = useQuery({
+    queryKey: ['notifications'],
+    queryFn: async () => {
+      try {
+        const response = await fetch('/api/notifications');
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error(data.error || 'Failed to get notifications');
+        }
+        return data;
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    },
+  });
+
+  const { mutate: deleteNotifications, isPending: isDeleting } = useMutation({
+    mutationFn: async () => {
+      try {
+        const response = await fetch('/api/notifications/', {
+          method: 'DELETE',
+        });
+        const data = await response.json();
+
+        if (!response.ok) {
+          throw new Error(data.error || 'Failed to delete notifications');
+        }
+        return data;
+      } catch (error) {
+        console.error(error);
+        throw error;
+      }
+    },
+    onSuccess: () => {
+      toast.success('Notifications deleted successfully');
+      queryClient.invalidateQueries({ queryKey: ['notifications'] });
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  const handleDeleteNotifications = () => {
+    if (isDeleting) {
+      return;
+    }
+    deleteNotifications();
   };
 
   return (
@@ -46,7 +74,9 @@ const NotificationPage = () => {
               className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <a onClick={deleteNotifications}>Delete all notifications</a>
+                <a onClick={handleDeleteNotifications}>
+                  Delete all notifications
+                </a>
               </li>
             </ul>
           </div>
