7f23a3983f1fcb1a8409081e2b651c0679c98654
Serhii Osa <oserdevit@gmail.com>
2025-01-31 16:45:34 +0200

    fixed server for deployment

--- backend/server.js
+++ backend/server.js
@@ -29,15 +29,15 @@ app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
 app.use('/api/notifications', notificationRoutes);
 
-if ((process.env.NODE_ENV = 'production')) {
-  app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
   });
 }
 
 app.listen(PORT, () => {
-  console.log('Server is running on http://localhost:' + PORT);
+  console.log('Server is running on port ' + PORT);
   connectMongoDB();
 });
